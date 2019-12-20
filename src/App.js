93bf5997c5d15459dbe9ef46e58b349ad2c92973
King Elisha <ek.chibueze@gmail.com>
2019-12-20 20:28:56 +0100

    html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Remarkable } from 'remarkable';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  state = {
+    markdownHTML: ""
+  }
+
+  markdownText = "";
+
+  /**
+   * @param {React.ChangeEvent<HTMLTextAreaElement>} e
+   */
+  onChange = (e) => {
+    this.markdownText = e.target.value;
+
+    const md = new Remarkable({
+      linkify: true
+    });
+    this.setState({
+      markdownHTML: md.render(this.markdownText)
+    });
+  };
+
+  render() {
+    const { markdownHTML } = this.state;
+    return (
+      <div className="App container">
+        <div className="content">
+          <div className="left">
+            <textarea
+              name="markdownText"
+              id=""
+              onChange={this.onChange}>
+
+            </textarea>
+          </div>
+
+          <div className="right">
+            <div
+              dangerouslySetInnerHTML={{ __html: markdownHTML }}></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
