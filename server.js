665253e52d4c139b21a377a597c54bdbb8851ddc
King Elisha <ek.chibueze@gmail.com>
2020-01-04 18:12:57 +0100

    convert file route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const markdownPdf = require('markdown-pdf');
 const fs = require('fs');
+const path = require('path');
 
 const server = express();
 
@@ -10,4 +11,16 @@ server.use(express.urlencoded({ extended: false }));
 server.post('/pdf', (req, res) => {
   const md = req.body.md;
 
+  const convertedFilePath = path.join(__dirname, 'converts', `md-to-pdf-${Date.now()}`);
+  markdownPdf().from(md)
+    .to(convertedFilePath, () => {
+      console.log("converted");
+      res.download(convertedFilePath, (err) => {
+        if (err) return console.log(err);
+
+        console.log("file downloaded");
+      })
+    });
 });
+
+server.listen('8000', () => console.log("Server Running"));
