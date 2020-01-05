75b741938486d319c5ce4200f6f45ca5786e342e
King Elisha <ek.chibueze@gmail.com>
2020-01-05 13:58:08 +0100

    download

--- server.js
+++ server.js
@@ -5,14 +5,24 @@ const path = require('path');
 
 const server = express();
 
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+server.use(allowCrossDomain);
+
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
-server.post('/pdf', (req, res) => {
+server.post('/pdf/:link', (req, res) => {
   const md = req.body.md;
+  const link = req.params.link;
 
-  const convertedFilePath = path.join(__dirname, 'converts', `md-to-pdf-${Date.now()}`);
-  markdownPdf().from(md)
+  const convertedFilePath = path.join(__dirname, 'converts', `md-to-pdf-${link}.pdf`);
+  markdownPdf().from.string(md)
     .to(convertedFilePath, () => {
       console.log("converted");
       res.download(convertedFilePath, (err) => {
